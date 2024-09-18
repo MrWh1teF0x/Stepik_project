ba4bd1fce88ebc1de411c5be7397e258f0a71ca0
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-09-19 00:28:50 +0300

    add primitive class "StepikAPI"

---
+++ src/StepikAPI/stepikAPI.py
@@ -0,0 +1,24 @@
+import requests
+from logged_session import LoggedSession
+
+
+class StepikAPI:
+    session: LoggedSession = None
+
+    def __init__(self, session: LoggedSession) -> None:
+        self.session = session
+
+    def login():
+        pass
+
+    def step_create():
+        pass
+
+    def step_delete():
+        pass
+
+    def step_update():
+        pass
+
+    def fetch_objects():
+        pass
