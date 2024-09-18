4131b50cb81891cef454cc1e4df442642ff11928
XehiDat <123zettachanel123@gmail.com>
2024-09-18 19:00:33 +0300

    open_file -> read_file

--- src/Parse_Classes/OnlineTokens.py
+++ src/Parse_Classes/OnlineTokens.py
@@ -1,7 +1,7 @@
 import _io
 import warnings
 
-from Parse_Classes.PageParsers import Page
+from src.Parse_Classes.PageParsers import Page
 
 
 class OnlineStep:
@@ -16,16 +16,15 @@ class OnlineStep:
 class OnlineLesson:
     id: int = None
     steps: list[OnlineStep] = []
-    file: _io.TextIOWrapper = None
+    file: list[str] = None
 
     def __init__(self, file_path: str = ""):
         if file_path == "":
-            self.open_file(file_path)
+            self.read_file(file_path)
 
-    def open_file(self, file_path: str):
+    def read_file(self, file_path: str):
         try:
-            file = open(file_path, "r+", encoding="UTF-8")
-            self.file = file
+            self.file = open(file_path, "r+", encoding="UTF-8").readlines()
         except FileNotFoundError or FileExistsError:
             warnings.warn(ImportWarning("File not found or doesn't exists"))
         except Exception:
