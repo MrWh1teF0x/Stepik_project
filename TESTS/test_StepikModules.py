604792bec1395de2ed6c9c8653b32ec12f868a9d
XehiDat <123zettachanel123@gmail.com>
2024-09-18 20:05:04 +0300

    added test for OnlineLesson initiation

--- TESTS/test_StepikModules.py
+++ TESTS/test_StepikModules.py
@@ -4,7 +4,24 @@ import pytest
 def test_Lesson_init():
     a1 = OnlineLesson()
 
+    # wrong calls ----------------------------------------------------------------
     assert a1.file is None
-    with pytest.warns(ImportWarning):
-        a1.open_file(r"D:\PyCharm Projects\Stepic_project\files\sadmple.md")
-
+    with pytest.warns(UserWarning):
+        a1.read_file(r"D:\PyCharm Projects\Stepic_project\files\sadmple_1.md")
+    assert a1.file is None
+    with pytest.warns(UserWarning):
+        a1.read_file(r"D:\.md")
+    with pytest.warns(UserWarning):
+        a1.read_file(r"")
+    assert a1.file is None
+    # correct call ---------------------------------------------------------------
+    a1.read_file(r"D:\PyCharm Projects\Stepic_project\files\test.md")
+    assert a1.file == ["# This is a test file\n", "it is only used for testing purposes\n", "\n",
+                       "* 1\n", "* 2\n", "* 3\n", "\n", "### Header 3\n"]
+    # wrong call, file not changed -----------------------------------------------
+    with pytest.warns(UserWarning):
+        a1.read_file(r"")
+    assert a1.file is not None
+    # clear ----------------------------------------------------------------------
+    a1 = OnlineLesson()
+    assert a1.file is None
