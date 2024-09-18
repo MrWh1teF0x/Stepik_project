4750f5bb4317ea4ff2206a6e475d2c64138e84c9
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-09-19 00:27:17 +0300

    add primitive class "LoggedSession"

---
+++ src/StepikAPI/logged_session.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+class LoggedSession:
+
+    def __init__(self) -> None:
+        self.__session = requests.Session()
+
+    def request():
+        pass
+
+    def headers():
+        pass
