287083380cf84c8d957081ae7b2c15f694246881
XehiDat <123zettachanel123@gmail.com>
2024-09-18 23:06:59 +0300

    added searching for format in line + changed check to match line from begining

--- src/Parse_Classes/RegExpFormats.py
+++ src/Parse_Classes/RegExpFormats.py
@@ -1,11 +1,19 @@
 import re
 
-format_lesson_id = re.compile(r"^lesson\s=\s[0-9]+")
-format_lesson_name = re.compile(r"^#\s.+")
+format_lesson_id = re.compile(r"^lesson\s+=\s+[0-9]+")
+format_lesson_name = re.compile(r"^#\s+.+")
+format_step_name = re.compile(r"^##\s+.+")
 
 
 def check_format(text: str, reg_exp: re.Pattern) -> tuple:
-    match: re.Match = reg_exp.search(text)
+    match = reg_exp.match(text)
+    if match is not None:
+        return match.span()
+    return ()
+
+
+def find_format(text: str, reg_exp: re.Pattern) -> tuple:
+    match = reg_exp.search(text)
     if match is not None:
         return match.span()
     return ()
