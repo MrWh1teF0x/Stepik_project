269bee7f2cfa2c9e8e377eee5b64f89f876ff09c
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-09-26 02:01:53 +0300

    add method "create_step" (primitive version)

--- src/StepikAPI/stepikAPI.py
+++ src/StepikAPI/stepikAPI.py
@@ -1,18 +1,29 @@
 import requests
+import json
 from logged_session import LoggedSession
 
 
 class StepikAPI:
     session: LoggedSession = None
+    url = "https://stepik.org/api/step-sources"
 
-    def __init__(self, session: LoggedSession) -> None:
+    def __init__(self, session: LoggedSession, token: str, cookie: str) -> None:
         self.session = session
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + token,
+            "Cookie": cookie,
+        }
 
     def login():
         pass
 
-    def step_create():
-        pass
+    def step_create(self, payload: dict):
+
+        responce = requests.post(
+            url=self.url, data=json.dumps(payload), headers=self.headers
+        )
+        print(responce.text)
 
     def step_delete():
         pass
