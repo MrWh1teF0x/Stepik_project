81edd1ec8693a20b0423a9ecf2481743f32585b7
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-09-26 15:26:19 +0300

    add token receipt

--- src/StepikAPI/logged_session.py
+++ src/StepikAPI/logged_session.py
@@ -3,12 +3,25 @@ import json
 
 
 class LoggedSession:
+    api_host = "https://stepik.org"
 
-    def __init__(self) -> None:
+    def __init__(self, client_id="", client_secret="") -> None:
         self.__session = requests.Session()
 
-    def request():
+        # Получаем токен
+        self.__auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+        response = requests.post(
+            f"{self.api_host}/oauth2/token/",
+            data={"grant_type": "client_credentials"},
+            auth=self.__auth,
+        )
+        self.__token = json.loads(response.text)["access_token"]
+
+    def request(self):
         pass
 
-    def headers():
+    def token(self):
+        return self.__token
+
+    def headers(self):
         pass
