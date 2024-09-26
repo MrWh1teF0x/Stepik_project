082b2d59ce585cf046bb9e4229e95362590c1551
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-09-26 15:36:36 +0300

    add cookie receipt

--- src/StepikAPI/logged_session.py
+++ src/StepikAPI/logged_session.py
@@ -4,6 +4,7 @@ import json
 
 class LoggedSession:
     api_host = "https://stepik.org"
+    course_host = "https://stepik.org/teach/courses"
 
     def __init__(self, client_id="", client_secret="") -> None:
         self.__session = requests.Session()
@@ -17,11 +18,18 @@ class LoggedSession:
         )
         self.__token = json.loads(response.text)["access_token"]
 
+        # Получаем cookie
+        cookie_responce = requests.get(self.course_host)
+        self.__cookie = f'csrftoken={cookie_responce.cookies["csrftoken"]}; sessionid={cookie_responce.cookies["sessionid"]}'
+
     def request(self):
         pass
 
     def token(self):
         return self.__token
+
+    def cookie(self):
+        return self.__cookie
 
     def headers(self):
         pass
