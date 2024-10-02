5b410f4aca296a279bb31347dd9c8e45b58a4acb
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-02 19:24:54 +0300

    add "__main__" file

---
+++ __main__.py
@@ -0,0 +1,11 @@
+from src.Parse_Classes.OnlineTokens import OnlineStep, OnlineLesson
+from src.Parse_Classes.PageParsers import Page, PageChoice, PageText
+from src.StepikAPI.stepikAPI import StepikAPI
+
+
+def main():
+    step = OnlineStep()
+
+
+if __name__ == "__main__":
+    main()
