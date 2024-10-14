753782cf40ee666c6ca12c2bd5a32070eb268725
XehiDat <123zettachanel123@gmail.com>
2024-10-14 13:13:52 +0300

    moved my debug to tests

--- TESTS/test_StepikModules.py
+++ TESTS/test_StepikModules.py
@@ -29,12 +29,31 @@ def test_Lesson_init():
     assert a1.file is None
 
 
-def test_RegExp_check_format():
-    f1 = re.compile("c")
-    f2 = re.compile("wadwadwad")
-    f3 = format_lesson_id
+def test_PyParse_check_format():
+    a_res = format_lesson_id.parseString("lesson = 123")
+    # нужен тест на пробелы
+    assert a_res.asList() == ["lesson", '=', '123']
+    assert a_res.asDict() == {'lesson_id': '123'}
+    b_res = format_lesson_name.parseString("#    Lesson 1      ")
+    assert b_res.asList() == ['#', 'Lesson 1      ']
+    assert b_res.asDict() == {'lesson_name': 'Lesson 1      '}
+    c_res = format_step_name.parseString("##\t\t\t dq21eoi1 231o231238")
+    assert c_res.asList() == ['##', 'dq21eoi1 231o231238']
+    assert c_res.asDict() == {'step_name': 'dq21eoi1 231o231238'}
 
-    # TODO: add tests for f1, f2, f3
+    a = format_lesson_id.runTests("lesson = 123", printResults=False)
+    assert a[0]
+    assert a[1][0][1].asList() == ['lesson', '=', '123']
+    assert a[1][0][1].asDict() == {'lesson_id': '123'}
+
+    b = format_lesson_name.runTests("  #    Lesson 1      ", comment=None, printResults=False)
+    assert b[0]
+    assert b[1][0][1].asList() == ['#', 'Lesson 1']
+    assert b[1][0][1].asDict() == {"lesson_name": 'Lesson 1'}
+
+    match_b = find_format("# lesson 1 \n # 123123123", format_lesson_name)
+    assert match_b[0][0].asList() == ['#', 'lesson 1 ']
+    assert match_b[1][0].asList() == ['#', '123123123']
 
 
 def test_Lesson_parse():
--- src/PyParseFormats.py
+++ src/PyParseFormats.py
@@ -8,7 +8,7 @@ class HiddenFormats:
     _hash_symb = pp.Literal('#')
     # ------------------------------------------------------------------------------------------------------------------
     # format_lesson_id      -> [("lesson", "=", *number*) {'lesson': *number*}]
-    f_les_id = pp.Literal("lesson") + "=" + pp.Word(pp.nums)("lesson")
+    f_les_id = pp.Literal("lesson") + "=" + pp.Word(pp.nums)("lesson_id")
     # format_lesson_name    -> [("#", *line_of_text*), {lesson_name: *line_of_text*}]
     f_les_name = pp.Keyword("#") + pp.Suppress(pp.White()) + (pp.restOfLine())("lesson_name")
     # format_step_name      -> [("##", *line_of_text*), {step_name: *line_of_text*}]
@@ -32,24 +32,3 @@ def find_format(text: str, parse_exp: pp.ParserElement) -> tuple:
 
 def match_format(text: str, parse_exp):
     pass
-
-
-"""
-a_res = format_lesson_id.parseString("lesson = 123")
-# нужен тест на пробелы
-print(f"{a_res=}")
-b_res = format_lesson_name.parseString("#    Lesson 1      ")
-print(f"{b_res=}")
-c_res = format_step_name.parseString("##\t\t\t dq21eoi1 231o231238")
-print(f"{c_res=}")
-print()
-
-a = format_lesson_id.runTests("lesson = 123", printResults=False)
-print(a)
-b = format_lesson_name.runTests("#    Lesson 1      ", comment=None, printResults=False)
-print(b)
-
-
-match_b = list(format_lesson_name.scanString("# lesson 1 \n # 123123123"))
-print(f"{match_b}")
-"""
