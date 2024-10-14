e0fbfefdb42a02276a8c32305d9c04a02d9087e5
XehiDat <123zettachanel123@gmail.com>
2024-10-14 12:21:59 +0300

    fixed format

--- src/PyParseFormats.py
+++ src/PyParseFormats.py
@@ -6,9 +6,9 @@ import re
 class HiddenFormats:
     _unexpected = pp.SkipTo(pp.LineEnd(), include=True)("Unexpected_string")
     _hash_symb = pp.Literal('#')
-    # ----------------------------------------------------------------------------------------------------------------------
+    # ------------------------------------------------------------------------------------------------------------------
     # format_lesson_id      -> [("lesson", "=", *number*) {'lesson': *number*}]
-    f_les_id = pp.Word("lesson") + pp.Keyword("=") + pp.Word(pp.nums)("lesson")
+    f_les_id = pp.Literal("lesson") + "=" + pp.Word(pp.nums)("lesson")
     # format_lesson_name    -> [("#", *line_of_text*), {lesson_name: *line_of_text*}]
     f_les_name = pp.Keyword("#") + pp.Suppress(pp.White()) + (pp.restOfLine())("lesson_name")
     # format_step_name      -> [("##", *line_of_text*), {step_name: *line_of_text*}]
@@ -36,6 +36,7 @@ def match_format(text: str, parse_exp):
 
 """
 a_res = format_lesson_id.parseString("lesson = 123")
+# нужен тест на пробелы
 print(f"{a_res=}")
 b_res = format_lesson_name.parseString("#    Lesson 1      ")
 print(f"{b_res=}")
