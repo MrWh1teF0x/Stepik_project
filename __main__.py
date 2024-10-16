2b0e8aa50c74aa809085948d7154c2cdbfaed346
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-17 02:26:50 +0300

    add reading "client_id" and "client_secret" from .yaml file

--- __main__.py
+++ __main__.py
@@ -1,10 +1,27 @@
 from src.Parse_Classes.OnlineTokens import OnlineStep, OnlineLesson
-from src.Parse_Classes.PageParsers import Page, PageChoice, PageText
-from src.StepikAPI.stepikAPI import StepikAPI
+
+from src.Parse_Classes.PageParsers import *
+from src.StepikAPI.stepik_api import StepikAPI, LoggedSession
+import json, requests, yaml
 
 
 def main():
-    step = OnlineStep()
+    client_id, client_secret = None, None
+
+    try:
+        with open("cred.yaml") as file:
+            s = yaml.load(file, yaml.SafeLoader)
+            client_id = s["client_id"]
+            client_secret = s["client_secret"]
+
+    except FileExistsError:
+        print('File "cred.yaml" does not exist!')
+
+    except KeyError:
+        print('There are no variables "client_id" or "client_secret"!')
+
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
