57697ea436115b8ad435b7c9189337a15c32efa1
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-22 01:22:27 +0300

    add func "setup_logger"

--- src/StepikAPI/logged_session.py
+++ src/StepikAPI/logged_session.py
@@ -1,5 +1,49 @@
 import requests
 import json
+import pathlib
+import logging
+
+
+LOGGER_NAME = "stepik"
+logger: logging.Logger = None
+
+
+def setup_logger(log_cli_level, log_file_level):
+    """
+    Use own logger for logging into out.log file and console(?)
+    """
+    global logger
+
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.setLevel(max(log_cli_level, log_file_level))
+
+    # create_by_type file handler which logs even debug messages
+
+    current = pathlib.Path(__file__).parent.parent.resolve()
+    full_filename = str(current.joinpath("out.log"))
+
+    fh = logging.FileHandler(full_filename, mode="w", encoding="utf-8")
+    fh.setLevel(log_file_level)
+
+    # create_by_type console handler with a higher log level
+    ch = logging.StreamHandler()
+    ch.setLevel(log_cli_level)
+
+    # create_by_type formatter and add it to the handlers
+    formatter_full = logging.Formatter(
+        "%(asctime)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s",
+        datefmt="%m-%d %H:%M:%S",
+    )
+    formatter_short = logging.Formatter(
+        "%(levelname)8s: %(message)s", datefmt="%m-%d %H:%M:%S"
+    )
+    # formatter = logging.Formatter('%(message)s')
+    fh.setFormatter(formatter_full)
+    # ch.setFormatter(formatter_short)
+
+    # add the handlers to the logger
+    # logger.addHandler(ch)
+    logger.addHandler(fh)
 
 
 class LoggedSession:
