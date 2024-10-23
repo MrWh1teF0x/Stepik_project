46e73da38a5f0c46d87b968dc4f186809105c4f0
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-23 17:26:54 +0300

    delete argument "session" from all methods

--- src/Parse_Classes/online_tokens.py
+++ src/Parse_Classes/online_tokens.py
@@ -2,9 +2,9 @@ import requests
 import json
 from dataclasses import dataclass, field
 
-
-from Parse_Classes.types_of_step import *
+from src.Parse_Classes.types_of_step import *
 from src.StepikAPI.logged_session import LoggedSession as Session
+from src.StepikAPI.logged_session import TypeRequest
 
 
 host = "https://stepik.org"
@@ -18,30 +18,28 @@ class OnlineStep:
     def info(self, session: Session):
         pass
 
-    def create(self, session: Session, step_data: TypeStep = None):
+    def create(self, step_data: TypeStep = None):
         if step_data:
             self.step_data = step_data
 
-        responce = requests.post(
-            url=self.url, json=self.step_data.body(), headers=session.headers()
+        session = Session()
+        responce = session.request(
+            TypeRequest.POST, self.url, json_data=self.step_data.body()
         )
 
         json_data = json.loads(responce.text)
         self.step_data.id = json_data["step-sources"][0]["id"]
 
-    def update(self, session: Session, step_data: TypeStep = None):
+    def update(self, step_data: TypeStep = None):
         if step_data:
             self.step_data = step_data
-        responce = requests.put(
-            url=f"{self.url}/{self.step_data.id}",
-            json=self.step_data.body(),
-            headers=session.headers(),
-        )
 
-    def delete(self, session: Session):
-        responce = requests.delete(
-            url=f"{self.url}/{self.step_data.id}", headers=session.headers()
-        )
+        session = Session()
+        session.request(TypeRequest.POST, self.url, json_data=self.step_data.body())
+
+    def delete(self):
+        session = Session()
+        session.request(TypeRequest.DELETE, url=self.url)
         self.step_data = None
 
 
@@ -51,6 +49,11 @@ class OnlineLesson:
     name: str = ""
     steps: list[OnlineStep] = field(default_factory=list)
     url = f"{host}/api/lessons"
+
+    def __post_init__(self):
+        if self.id:
+            session = Session()
+            responce = session.request(TypeRequest.GET, url=f"{self.url}/{self.id}")
 
     def add_step(self, step: OnlineStep, position: int = 0):
         if not (0 <= position <= len(self.steps) - 1 or position == 0):
@@ -64,21 +67,21 @@ class OnlineLesson:
             else:
                 self.steps.insert(position + 1, step)
 
-    def update(self, session: Session, steps: list[OnlineStep]):
+    def update(self, steps: list[OnlineStep]):
         new_step_ids = [step.step_data.id for step in steps]
         old_step_ids = []
 
         for old_step in self.steps:
             if old_step.step_data.id not in new_step_ids:
-                old_step.delete(session)
+                old_step.delete()
             else:
                 old_step_ids.append(old_step.step_data.id)
 
         for step in steps:
             if step.step_data.id not in old_step_ids:
-                step.create(session)
+                step.create()
             else:
-                step.update(session)
+                step.update()
 
     def get_steps_ids(self, session: Session):
         responce = requests.get(url=f"{self.url}/{self.id}", headers=session.headers())
