74eab2d6a93924635c55b3cb4db096aa7adda711
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-30 19:43:34 +0300

    beautify

--- src/StepikAPI/logged_session.py
+++ src/StepikAPI/logged_session.py
@@ -6,14 +6,15 @@ import pathlib
 import yaml
 from enum import Enum
 
+path = pathlib.Path(__file__).parent
 
-path = pathlib.Path(__file__).parent / "../../cred.yaml"
+cred_path = path / "../../cred.yaml"
 client_id, client_secret = None, None
 
 
 def init_secret_fields() -> None:
     try:
-        with path.open("r") as file:
+        with cred_path.open("r") as file:
             s = yaml.load(file, yaml.SafeLoader)
             global client_id, client_secret
             client_id = s["client_id"]
@@ -42,32 +43,21 @@ def setup_logger(log_cli_level, log_file_level) -> None:
     logger = logging.getLogger(LOGGER_NAME)
     logger.setLevel(max(log_cli_level, log_file_level))
 
-    # create_by_type file handler which logs even debug messages
+    log_path = path / "../out.log"
 
-    current = pathlib.Path(__file__).parent.parent.resolve()
-    full_filename = str(current.joinpath("out.log"))
-
-    fh = logging.FileHandler(full_filename, mode="w", encoding="utf-8")
+    fh = logging.FileHandler(log_path, mode="w", encoding="utf-8")
     fh.setLevel(log_file_level)
 
-    # create_by_type console handler with a higher log level
     ch = logging.StreamHandler()
     ch.setLevel(log_cli_level)
 
-    # create_by_type formatter and add it to the handlers
+    # Формат вывода информации о запросах в файл out.log
     formatter_full = logging.Formatter(
         "%(asctime)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s",
         datefmt="%m-%d %H:%M:%S",
     )
-    formatter_short = logging.Formatter(
-        "%(levelname)8s: %(message)s", datefmt="%m-%d %H:%M:%S"
-    )
-    # formatter = logging.Formatter('%(message)s')
     fh.setFormatter(formatter_full)
-    # ch.setFormatter(formatter_short)
 
-    # add the handlers to the logger
-    # logger.addHandler(ch)
     logger.addHandler(fh)
 
 
@@ -85,14 +75,14 @@ class LoggedSession:
     ) -> None:
         self.__session = requests.Session()
 
-        # что именно логировать в запросе и ответе
+        # Флаги, которые отвечают за то, что именно логировать в запросе и ответе
         self.log_url = log_url
         self.log_header = log_header
         self.log_data = log_data
         self.log_http_code = log_http_code
         self.log_resp_data = log_resp_data
 
-        # игнорировать ошибки ssl сертификата
+        # Флаг для игнорирования ошибки ssl сертификата
         self.ignore_ssl_certificate_errors = False
 
         global client_id, client_secret
