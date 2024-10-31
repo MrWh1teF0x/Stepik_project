a9aa12cf1ee33647cdee7c79d455399fe7b78c64
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-31 15:37:36 +0300

    add class "TaskTest" and add body to "StepTask"

--- src/Parse_Classes/types_of_step.py
+++ src/Parse_Classes/types_of_step.py
@@ -129,9 +129,17 @@ class StepString(TypeStep):
 
 
 @dataclass
+class TaskTest:
+    input: str = ""
+    output: str = ""
+
+
+@dataclass
 class StepTask(TypeStep):
-    code: str = ""
-    test_cases: list[str] = field(default_factory=list)
+    samples_count: int = 1
+    execution_time_limit: int = 5
+    execution_memory_limit: int = 256
+    test_cases: list[TaskTest] = field(default_factory=list)
 
     def body(self) -> dict:
         return {
@@ -140,9 +148,20 @@ class StepTask(TypeStep):
                     "name": "code",
                     "text": self.text,
                     "source": {
-                        "code": self.code,
-                        "samples_count": len(self.test_cases),
-                        "test_cases": [self.test_cases],
+                        "code": "def generate():\n    return []\n\ndef check(reply, clue):\n    return reply.strip() == clue.strip()\n",
+                        "samples_count": self.samples_count,
+                        "execution_time_limit": self.execution_time_limit,
+                        "execution_memory_limit": self.execution_memory_limit,
+                        "templates_data": "",
+                        "is_time_limit_scaled": True,
+                        "is_memory_limit_scaled": True,
+                        "is_run_user_code_allowed": True,
+                        "manual_time_limits": [],
+                        "manual_memory_limits": [],
+                        "test_archive": [],
+                        "test_cases": [
+                            [test.input, test.output] for test in self.test_cases
+                        ],
                     },
                 },
                 "lesson": self.lesson_id,
