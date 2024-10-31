1dbcc0b1adcb167b4f0c1040bbf7f4efe4b291a7
MrWh1teF0x <142975991+MrWh1teF0x@users.noreply.github.com>
2024-10-31 16:54:54 +0300

    add test_add_SteText

--- TESTS/test_lesson_methods.py
+++ TESTS/test_lesson_methods.py
@@ -13,3 +13,17 @@ STEP_IDS = [6166762, 6165951, 6166824, 6170935, 6170948]
 def test_get_steps_ids():
     lesson = OnlineLesson(id=LESSON_ID)
     assert lesson.get_steps_ids() == STEP_IDS
+
+
+def test_add_StepText():
+    lesson = OnlineLesson(id=LESSON_ID)
+
+    step_text = OnlineStep(
+        StepText(
+            text="Этот шаг был создан с помощью теста!", lesson_id=LESSON_ID, position=6
+        )
+    )
+    lesson.add_step(step_text)
+
+    step_text_2 = OnlineStep(id=lesson.get_steps_ids()[-1])
+    assert step_text_2.info() == step_text.info()
